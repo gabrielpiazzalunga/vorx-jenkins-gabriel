d9fbc713396949ea678f160d4b3e73227e276eb5
gabrielpiazzalunga <gabriel.piazzalunga@gmail.com>
2021-11-20 15:42:56 -0300

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "=========================="
+echo "================BUILDING VORX DOCKER IMAGE=========================="
+echo "=========================="
+docker-compose build --no-cache
