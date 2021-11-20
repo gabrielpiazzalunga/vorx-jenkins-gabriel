f56a1d5fcadeec8452280b6eef4d94bb570a2954
gabrielpiazzalunga <gabriel.piazzalunga@gmail.com>
2021-11-20 16:08:27 -0300

    Update push.sh

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ echo "** Pushing image ***"
 echo "********************"
 
 echo "** Logging in ***"
-docker login -u vcavalcanti -p $HUB_PASSWORD
+docker login -u gpiazza94 -p $HUB_PASSWORD
 
 echo "*** Pushing image ***"
-docker push vcavalcanti/jenkins-vorx:$BUILD_TAG
+docker push gpiazza94/jenkins-vorx:$BUILD_TAG
